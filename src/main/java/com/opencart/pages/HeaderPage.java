ce47d2d103378765e7155604cf182b998f8f3e30
Selengiks <maksiimys1980@gmail.com>
2021-11-18 17:05:09 +0200

    minor fix

--- src/main/java/com/opencart/pages/HeaderPage.java
+++ src/main/java/com/opencart/pages/HeaderPage.java
@@ -12,6 +12,14 @@ public class HeaderPage extends BasePage {
     @FindBy(xpath = ".//*[contains(@href,'register')]")
     private WebElement registerButton;
 
+    //Header search field
+    @FindBy(xpath = ".//*[@id='search']/input")
+    private WebElement searchStr;
+
+    //Header search field button
+    @FindBy(xpath = ".//*[@id='search']/span/button")
+    private WebElement searchStrSub;
+
     public WebElement getMyAccountButton() {
         wait.until(ExpectedConditions.visibilityOf(myAccountButton));
         return myAccountButton;
@@ -19,5 +27,13 @@ public class HeaderPage extends BasePage {
 
     public WebElement getRegisterButton() {
         return registerButton;
+    }
+
+    public WebElement getSearch() {
+        return searchStr;
+    }
+
+    public WebElement getSearchStrSub() {
+        return searchStrSub;
     }
 }
