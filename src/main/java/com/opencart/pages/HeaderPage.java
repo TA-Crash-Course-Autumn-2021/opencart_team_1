bc2c4a55039b6c8da8fefee07238d486fd78d7d6
Selengiks <maksiimys1980@gmail.com>
2021-11-22 17:16:38 +0200

    Minor code improvment (change getter constructions on @Getter) Announcement 10, 11 and 13 issues

--- src/main/java/com/opencart/pages/HeaderPage.java
+++ src/main/java/com/opencart/pages/HeaderPage.java
@@ -1,9 +1,11 @@
 package com.opencart.pages;
 
+import lombok.Getter;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+@Getter
 public class HeaderPage extends BasePage {
 
     @FindBy(xpath = ".//*[@title='My Account']")
@@ -23,17 +25,5 @@ public class HeaderPage extends BasePage {
     public WebElement getMyAccountButton() {
         wait.until(ExpectedConditions.visibilityOf(myAccountButton));
         return myAccountButton;
-    }
-
-    public WebElement getRegisterButton() {
-        return registerButton;
-    }
-
-    public WebElement getSearch() {
-        return searchStr;
-    }
-
-    public WebElement getSearchStrSub() {
-        return searchStrSub;
     }
 }
